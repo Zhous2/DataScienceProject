26f853a0938dbc4eb21bd38517af223725b6573f
HasagiAseryo <huynha5@vcu.edu>
2018-11-14 10:54:28 -0500

    used file to add classes to csv files.  New folder with complete csv files

--- AddClass.java
+++ AddClass.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 public class AddClass {
 
     public static void main(String[] args) {
-        String featureGenerationFileName = "";
-        String outputFileName = "";
+//        String featureGenerationFileName = "autoGearyFull.csv";
+//        String outputFileName = "autoGearyComplete.csv";
 
-        ArrayList<String> featureGenerationResults = parseFile("featureGenerationFileName");
+        ArrayList<String> featureGenerationResults = parseFile("autoMoranFull.csv"); //add csv file here
         ArrayList<String> classList = parseFileGetClass("acidData.csv");
 
         ArrayList<String> featureGenerationWithClass = combineFeatureGenerationWithClassList(featureGenerationResults, classList);
 
-        writeToFile(featureGenerationWithClass,"outputFileName");
+        writeToFile(featureGenerationWithClass,"autoMoranComplete.csv");  //output name you want
     }
 
     private static ArrayList<String> combineFeatureGenerationWithClassList(ArrayList<String> featureGenerationResults, ArrayList<String> classList) {
